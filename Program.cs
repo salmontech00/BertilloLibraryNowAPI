e6ba4f75f918df26f583bf1b35bb27e0c3b724cc
salmontech00 <015mhikebertillo@gmail.com>
2026-04-24 15:12:43 +0800

    test

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://+:{port}");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
